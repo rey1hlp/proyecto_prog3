e1a0ac6ae61388f5103fe553d43f4015fbfdfb56
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-08 23:48:44 -0500

    v2

--- src/Administrador.java
+++ src/Administrador.java
@@ -6,24 +6,24 @@ public class Administrador extends Usuario{
 		//super(DNI,nombre,apellidoPaterno,apellidoMaterno,telefono,estado);
 	}
 	@Override
-	void consultarDatos(){
+	public void consultarDatos(){
 		//Sería bueno que el administrador tuviera atributos propios
 	}
-	void registrarEmpleado(Empleado empleado){
+	public void registrarEmpleado(Empleado empleado){
 		//Registra empleado en la base de datos
 	}
-	void modificarEstadoEmpleado(int idUsuario,EstadoUsuario estadoUsuario){
+	public void modificarEstadoEmpleado(int idUsuario,EstadoUsuario estadoUsuario){
 		//Cambia el estado del empleado como activo o inactivo
 	}
-	void editarPerfilPropio(Administrador administrador){
+	public void editarPerfilPropio(Administrador administrador){
 		//El objeto administrador encapsula los datos a modificar del admin
 		//Aquí se hace los setter o un 
 	}         
-	void editarPerfilEmpleado(int idUsuario,Empleado empleado){
+	public void editarPerfilEmpleado(int idUsuario,Empleado empleado){
 		//Buscara en el array de empleados y editará la info de un empleado
 		//El objeto empleado enviado lleva los datos a editra del iésimo empleado
 	}
-	void consultarRendimientoEmpleados(Date fechaInicio, Date fechaFin){
+	public void consultarRendimientoEmpleados(Date fechaInicio, Date fechaFin){
 		//Genera el reporte de empleados según una fecha de inicio y una fecha final establecidas
 	}
 }
