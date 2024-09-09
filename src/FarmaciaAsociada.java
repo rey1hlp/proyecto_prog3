e05fbf223e0b8ece62aea76885ed9bfdf89da698
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-08 23:48:17 -0500

    v2

--- src/FarmaciaAsociada.java
+++ src/FarmaciaAsociada.java
@@ -1,3 +1,13 @@
 public class FarmaciaAsociada extends EmpresaEntrega {
 	
+	//private int ID;
+	//private String nombre;
+	//private Departamento departamento;
+	//private String direccion;
+	//private int telefono;
+	
+	@Override
+	public void consultarDatos(){
+		
+	}
 }
