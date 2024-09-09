94e0219b84a5065dc328c4905a6cc4b05f44175f
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-08 22:32:50 -0500

    version 2

--- src/Tecnico.java
+++ src/Tecnico.java
@@ -2,4 +2,12 @@ import java.util.ArrayList;
 public class Tecnico extends Empleado{
 	private FarmaciaPropia farmacia;
 	private ArrayList<PedidoPropio> pedidosGenerados;
+
+	public void atenderPedido(int ID);
+	public void descartarPedido(int ID);
+	public void llenarMedicinas();
+	public void llenarDatosPoliza();
+	public void consultarDatosPoliza();
+	public void registrarDireccionEnvio();
+	public void subirDocumentoPago();
 }
