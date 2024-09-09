38a5b4b3a40e79df176ed3531ad17e9fa0d027fd
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-08 23:07:01 -0500

    v2

--- src/Poliza.java
+++ src/Poliza.java
@@ -1,9 +1,13 @@
 import java.util.Date;
-public class Poliza{
+public class Poliza implements IConsultable {
 	private int codigo;
 	private Date fechaEmision;
 	private Date fechaVencimiento;
 	private double limiteCobertura;
+	private double porcentajeCobertura;
 	private String aseguradora;
 	private char estado;
+
+	public double calcularCobertura(double monto);
+	
 }
