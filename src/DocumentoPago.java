983ec06b741fca924b55f6894ef8ca0a68bf51b3
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-08 23:46:41 -0500

    v1.2

--- src/DocumentoPago.java
+++ src/DocumentoPago.java
@@ -1,4 +1,9 @@
 public class DocumentoPago {
 	private int numOperacion;
 	private double montoPagado;
+
+	public DocumentoPago(int numOperacion, double montoPagado) {
+		this.numOperacion = numOperacion;
+		this.montoPagado = montoPagado;
+	}
 }
