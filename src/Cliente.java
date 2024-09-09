bbff327949c29817b6b8a11b33451c1115b492fc
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-08 23:29:15 -0500

    v1.2

--- src/Cliente.java
+++ src/Cliente.java
@@ -10,4 +10,11 @@ public class Cliente implements IConsultable {
 	private Date fechaNacimiento;
 	private boolean tienePoliza;
 	private Poliza poliza;
+
+	public Cliente(int DNI, String nombre, String apellidoPaterno, String apellidoMaterno) {
+		this.DNI = DNI;
+		this.nombre = nombre;
+		this.apellidoPaterno = apellidoPaterno;
+		this.apellidoMaterno = apellidoMaterno;
+	}	
 }
