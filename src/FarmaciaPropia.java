ccd555739c1bf64dba3a0dd5d8a71e0751e1ec75
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-09 14:32:31 -0500

    v1.2

--- src/FarmaciaPropia.java
+++ src/FarmaciaPropia.java
@@ -4,4 +4,14 @@ public class FarmaciaPropia extends EmpresaEntrega {
 	private ArrayList<Tecnico> tecnicos;
 	private ArrayList<PedidoPropio> pedidosEncargados;
 	private ArrayList<MedicinaPropia> medicinasPropias;
+
+	public FarmaciaPropia(int ID, String nombre, String direccion, int telefono,
+			     Departamento departamento) {
+		// ....
+	}
+
+	public agregarPedido(Solicitud solicitud) {
+		// toma una solicitud y la transforma a un pedido que será procesado
+		// por la farmacia y atendido por un técnico de esta
+	}
 }
