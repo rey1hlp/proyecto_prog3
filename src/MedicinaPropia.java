b1feac9354c7ff286ef4a6ae72dbcc7d5a6a020c
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-09 14:37:01 -0500

    v1.2

--- src/MedicinaPropia.java
+++ src/MedicinaPropia.java
@@ -1,5 +1,9 @@
-public class MedicinaPropia {
+public class MedicinaPropia implements IConsultable {
 	private MedicinaAsociada medicinaAsociada;
 	private int ID;
 	private double precio;
+
+	public MedicinaPropia(MedicinaAsociada medicinaAsociada, int ID, double precio) {
+		// ...
+	}
 }
