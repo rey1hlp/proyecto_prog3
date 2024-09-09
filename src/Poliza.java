52867a78e7835246d400c2a8e06da5f435e4a3d5
elm1smo <168689609+elm1smo@users.noreply.github.com>
2024-09-08 23:45:22 -0500

    v1.2

--- src/Poliza.java
+++ src/Poliza.java
@@ -6,8 +6,22 @@ public class Poliza implements IConsultable {
 	private double limiteCobertura;
 	private double porcentajeCobertura;
 	private String aseguradora;
-	private char estado;
+	private boolean estaVigente;
+
+	public Poliza(int codigo, Date fechaEmision, Date fechaVencimiento,
+		     double limiteCobertura, double porcentajeCobertura, String aseguradora) {
+		this.codigo = codigo;
+		this.fechaEmision = fechaEmision;
+		this.fechaVencimiento = fechaVencimiento;
+		this.limiteCobertura = limiteCobertura;
+		this.porcentajeCobertura = porcentajeCobertura;
+		this.aseguradora = aseguradora;
+
+		// el atributo estaVigente se determinaría con la comparacion de fecha
+		// actual con la de vencimiento
+	}
 
 	public double calcularCobertura(double monto);
-	
+
+	public void consultarDatos();
 }
