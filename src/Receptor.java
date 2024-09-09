0a7f8c46217901e117cff360a068bfb221de0e2b
Jean-RF <168687354+Jean-RF@users.noreply.github.com>
2024-09-08 23:00:42 -0500

    v1.2

--- src/Receptor.java
+++ src/Receptor.java
@@ -1,4 +1,11 @@
 import java.util.ArrayList;
 public class Receptor extends Empleado{
 	private ArrayList<Solicitud> solicitudesRecibidas;
+	public void atenderSolicitud();
+	public void eliminarSolicitud();
+	public void llenarDatosPaciente();
+	public void cargarDocumentos();
+	public void asignarFarmacia();      //Lima o provincia
+	public void generarPedido(); //
+
 }
