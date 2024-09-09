cbe43af0748825a9e0dcfbda8fdc3fa8b9cd63bd
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-08 22:37:57 -0500

    v2

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,13 +1,13 @@
 public class Administrador extends Usuario{
-	
-	void registrarEmpleado();
-	void modificarEstadoEmpleado();
-	void editarPerfilPropio();
+
+	public Administrador(int DNI,String nombre,String apellidoPaterno,String apellidoMaterno,int telefono,EstadoUsuario estado){
+		//super(DNI,nombre,apellidoPaterno,apellidoMaterno,telefono,estado);
+	}
+
+	void registrarEmpleado();         //Registra empleado en la base de datos
+	void modificarEstadoEmpleado();      //Modifica datos del empleado en la base de datos
+	void editarPerfilPropio();         
 	void editarPerfilEmpleado();
 	void consultarRendimientoEmpleados();
-
-	public Administrador(){
-		super();
-	}
 	
 }
