f1a4bf61e825a15d9e3c51e5ceaf65122bf0daf1
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-08 22:21:30 -0500

    Update Administrador.java
    Hola mundo

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,3 +1,8 @@
 public class Administrador extends Usuario{
-	 
+	void registrarEmpleado();
+	void modificarEstadoEmpleado();
+	void editarPerfilPropio();
+	void editarPerfilEmpleado();
+	void consultarRendimientoEmpleados();
+
 }
