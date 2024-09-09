f75e51295eb2a7bee4ec0937d0485b228aee3ca7
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-08 23:28:17 -0500

    v2

--- src/Administrador.java
+++ src/Administrador.java
@@ -5,7 +5,10 @@ public class Administrador extends Usuario{
 	public Administrador(int DNI,String nombre,String apellidoPaterno,String apellidoMaterno,int telefono,EstadoUsuario estado){
 		//super(DNI,nombre,apellidoPaterno,apellidoMaterno,telefono,estado);
 	}
-
+	@Override
+	void consultarDatos(){
+		//Sería bueno que el administrador tuviera atributos propios
+	}
 	void registrarEmpleado();         //Registra empleado en la base de datos
 	void modificarEstadoEmpleado(int idUsuario,EstadoUsuario estadoUsuario);      //Modifica datos del empleado en la base de datos
 	void editarPerfilPropio();         
