44164eac34d8de29df029cb533fdaf6e6ac7b660
Jean-RF <168687354+Jean-RF@users.noreply.github.com>
2024-09-08 23:22:48 -0500

    v2

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,3 +1,7 @@
 public abstract class Empleado extends Usuario{
+	private static int correlativo=1;
 	private int idUsuario;
+	public Empleado(int DNI,String nombre,String apellidoPaterno,String apellidoMaterno,int telefono,EstadoUsuario estado){
+		
+	}
 }
