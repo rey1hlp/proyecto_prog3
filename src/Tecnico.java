9751dce986dccaa8a4fc7c1f029fe6ea61e60fcb
rey1hlp <156461105+rey1hlp@users.noreply.github.com>
2024-09-10 20:38:42 -0500

    vR

--- src/Tecnico.java
+++ src/Tecnico.java
@@ -18,7 +18,7 @@ public class Tecnico extends Empleado{
 
 	@Override
 	void consultarDatos();
-	public void modificarDetalleEnvio(int idPedidoPropio, DetalleEnvio detalleEnvio){   //AAA
+	public void modificarDetalleEnvio(int ID, DetalleEnvio detalleEnvio){   //AAA
 		//EL técnico puede modificar el detalle de envío de un pedido 
 	}
 }
